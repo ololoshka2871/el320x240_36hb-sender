1d6f666987f2328fd38ebea0ff06010bcc7d79d2
ololoshka2871 <SupanKamarov@alumni.nottingham.ac.uk>
2026-01-05 16:58:04 +0300

    basicaly working

--- src/bin/rtmp-server.rs
+++ src/bin/rtmp-server.rs
@@ -1,54 +1,164 @@
-use ez_ffmpeg::rtmp::embed_rtmp_server::EmbedRtmpServer;
-use ez_ffmpeg::{FfmpegContext, Input, Output};
+use ez_ffmpeg::{AVRational, FfmpegContext, Input, Output};
 
-fn main() {
-    // Method 1: Using Embedded RTMP Server (Requires enabling `rtmp` feature)
+use const_format::concatcp;
 
-    // 1. Create and start an embedded RTMP server on "localhost:1935"
-    let mut embed_rtmp_server = EmbedRtmpServer::new("localhost:1935");
-    let embed_rtmp_server = embed_rtmp_server.start().unwrap(); // Start the RTMP server
+use serialport::SerialPort;
+use structopt::StructOpt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-    // 2. Create an RTMP "input" stream with app_name="my-app" and stream_key="my-stream"
-    //    This returns an `Output` that FFmpeg can push data into.
-    let output = embed_rtmp_server
-        .create_rtmp_input("my-app", "my-stream")
-        .unwrap();
+pub const DISPLAY_WIDTH: u32 = 320;
+pub const DISPLAY_HEIGHT: u32 = 240;
+pub const DISPLAY_FPS: u32 = 30;
 
-    // 3. Prepare an `Input` pointing to a local file (e.g., "test.mp4")
-    let input: Input = Input::from("N:/Anime/[AniPlague] Food Court de Mata Ashita S01 1080p/[AniPlague] Food Court de Mata Ashita - 01.mkv").set_readrate(1.0); // Optional: limit reading speed
+#[allow(unused)]
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Send rtmp stream from OBS el320x240_36hb over virtual serial port")]
+pub struct Cli {
+    /// width
+    #[structopt(long, default_value = concatcp!(DISPLAY_WIDTH))]
+    pub width: u32,
+
+    /// heigth
+    #[structopt(long, default_value = concatcp!(DISPLAY_HEIGHT))]
+    pub heigth: u32,
+
+    /// fps
+    #[structopt(long, default_value = concatcp!(DISPLAY_FPS))]
+    pub fps: u32,
+
+    /// RTMP port
+    #[structopt(short, default_value = "9009")]
+    pub rtmp_port: u16,
+
+    /// Serial port
+    #[structopt(short, default_value = "/dev/ttyACM0")]
+    pub serial_port: String,
+
+    /*
+    /// use filter algorithm
+    #[structopt(short, default_value = "Threshold")]
+    pub filter_algorithm: DitherAlgorithm,
+    */
+    /// level of black color, 0.0-1.0
+    #[structopt(long, default_value = "0.0", parse(try_from_str = parse_h_val))]
+    pub black_lvl: f32,
+
+    /// level of white color, 0.0-1.0
+    #[structopt(long, default_value = "1.0", parse(try_from_str = parse_h_val))]
+    pub white_lvl: f32,
+}
+
+fn parse_h_val(s: &str) -> Result<f32, String> {
+    let val = s.parse::<f32>().map_err(|e| e.to_string())?;
+    if val < 0.0 || val > 1.0 {
+        Err(format!("Value must be in range 0.0-1.0, got {val}"))
+    } else {
+        Ok(val)
+    }
+}
+
+#[tokio::main]
+async fn main() -> ez_ffmpeg::error::Result<()> {
+    let args = Cli::from_args();
+
+    // 1. Prepare an `Input` pointing to a local file (e.g., "test.mp4")
+    let input: Input = Input::from(format!("rtmp://127.0.0.1:{}", args.rtmp_port))
+        .set_readrate(1.0)
+        .set_input_opt("listen", "1")
+        .set_input_opt("flags", "low_delay");
+
+    // 2. todo: filters from config, like "diter" and so on
+    let filter = format!("format=gray");
+
+    let (mut reader, mut writer) = tokio::io::simplex(150 * 1024); // Specify a buffer capacity
+
+    // 3. output: Define the write callback for custom output handling
+    let write_callback = move |buf: &[u8]| -> i32 {
+        if buf.is_empty() {
+            return 0;
+        }
+
+        match futures::executor::block_on(writer.write_all(buf)) {
+            Err(_e) => ffmpeg_sys_next::AVERROR(ffmpeg_sys_next::AVFMT_FLAG_NOBUFFER),
+            Ok(_) => buf.len() as i32,
+        }
+    };
+
+    // https://ffmpeg.org/ffmpeg-formats.html#rawvideo
+    let output = Output::new_by_write_callback(write_callback)
+        .set_format("rawvideo")
+        .set_framerate(AVRational {
+            num: args.fps as i32,
+            den: 1,
+        })
+        .set_video_codec_opts(vec![
+            ("framerate", &format!("{}", args.fps)),
+            ("video_size", &format!("{}x{}", args.width, args.heigth)),
+        ])
+        .set_bits_per_raw_sample(8);
+
+    let mut port = serialport::new(&args.serial_port, 15000000)
+        .timeout(tokio::time::Duration::from_millis(5))
+        .open()
+        .expect("Failed to open serial port");
+
+    tokio::spawn(async move {
+        let mut buf = Vec::with_capacity(args.width as usize * args.heigth as usize);
+        let mut bit_buf = Vec::with_capacity(buf.capacity() / 8);
+        buf.resize(buf.capacity(), 0u8);
+        bit_buf.resize(bit_buf.capacity(), 0u8);
+
+        loop {
+            match reader.read_exact(&mut buf).await {
+                Ok(_) => {
+                    buf.chunks_exact(8).enumerate().for_each(|(i, chunk)| {
+                        let mut byte: u8 = 0;
+                        for (j, &pixel) in chunk.iter().enumerate() {
+                            let bit = if pixel > 128 { 1 } else { 0 };
+                            byte |= bit << (7 - j);
+                        }
+                        bit_buf[i] = byte;
+                    });
+                    display_send(port.as_mut(), (args.width, args.heigth), &bit_buf).await;
+                }
+                Err(e) => {
+                    eprintln!("Error reading from pipe: {}", e);
+                    break;
+                }
+            }
+        }
+    });
 
     // 4. Build and run the FFmpeg context
     FfmpegContext::builder()
         .input(input)
         .output(output)
-        .build()
-        .unwrap()
-        .start()
-        .unwrap()
-        .wait()
-        .unwrap();
+        .filter_desc(filter)
+        .build()?
+        .start()?
+        .await
+}
 
-    // Method 2: Using External RTMP Server (No need for `rtmp` feature)
+async fn display_send<P: SerialPort + ?Sized>(port: &mut P, (w, h): (u32, u32), frame: &[u8]) {
+    use bytes::{BufMut, BytesMut};
 
-    // 1. Prepare an `Input` pointing to a local file (e.g., "test.mp4")
-    let input: Input = Input::from("../../test.mp4").set_readrate(1.0); // Optional: limit reading speed
+    let line_size_bytes = (w / 8) as usize;
+    assert!(
+        frame.len() == line_size_bytes * h as usize,
+        "Frame size does not match specified width and height"
+    );
 
-    // 2. Output the stream to an external RTMP server
-    //    In this case, the stream is pushed to "rtmp://localhost/my-app/my-stream"
-    let output = Output::from("rtmp://localhost/my-app/my-stream")
-        .set_format("flv")
-        .set_video_codec("h264")
-        .set_audio_codec("aac")
-        .set_format_opt("flvflags", "no_duration_filesize");
+    frame
+        .chunks_exact(line_size_bytes)
+        .enumerate()
+        .for_each(|(line, data)| {
+            let offset_bytes = line * line_size_bytes;
+            let mut buf: BytesMut = BytesMut::new();
 
-    // 3. Build and run the FFmpeg context for external RTMP streaming
-    FfmpegContext::builder()
-        .input(input)
-        .output(output)
-        .build()
-        .unwrap()
-        .start()
-        .unwrap()
-        .wait()
-        .unwrap();
+            // offset in bytes
+            buf.put_u32_le(offset_bytes as u32);
+            buf.put_slice(data);
+
+            port.write_all(&buf).unwrap();
+        });
 }
